1f872a305f9fa81cfc775820cd50146a0327d0b0
ruchi06 <ruchi06@gmail.com>
2023-01-20 19:52:43 +0530

    Permutations

--- AlgorithamProblem/Permutation.cs
+++ AlgorithamProblem/Permutation.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace AlgorithamProblem
 {
-    internal class Permutation
+    public class Permutation
     {
+ 
+        // Recursion Method
+        public static void RecursionPermutation(string str, int initialPositionStr, int Lengthstr)
+        {
+            if (initialPositionStr == Lengthstr)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = initialPositionStr; i <= Lengthstr; i++)
+                {
+                    str = Swap(str, initialPositionStr, i);
+                    RecursionPermutation(str, initialPositionStr + 1, Lengthstr);
+                    str = Swap(str, initialPositionStr, i);
+                }
+            }
+        }
+        public static string Swap(string str, int position1, int position2)
+        {
+            char temp;
+
+            char[] charArray = str.ToCharArray();
+
+            temp = charArray[position1];
+            charArray[position1] = charArray[position2];
+            charArray[position2] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+        //Permutation in Iterative Way
+        public static void IterationPermutation(string str)
+        {
+            int number = str.Length;
+            int fact = Factorial(number);
+
+            for (int i = 0; i < fact; i++)
+            {
+                StringBuilder stringBuilder = new StringBuilder(str);
+                int temp = i;
+
+                for (int div = number; div >= 1; div--)
+                {
+                    int quotient = temp / div;
+                    int remiander = temp % div;
+
+                    Console.Write(stringBuilder[remiander]);
+                    stringBuilder.Remove(remiander, 1);
+                    temp = quotient;
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int Factorial(int number)
+        {
+            int val = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                val *= i;
+            }
+            return val;
+        }
     }
 }
+    
+
--- AlgorithamProblem/UserInput.cs
+++ AlgorithamProblem/UserInput.cs
@@ -6,6 +6,7 @@ namespace AlgorithamProblem
     {
         public void UserInputForPrograms()
         {
+            Console.WriteLine("Enter - 1. Permutation ");
             Console.WriteLine("Enter - 2. Binary Search ");
             Console.WriteLine("Choose - 3. Insertion Sort ");
             Console.WriteLine("Choose - 4. Bubble Sort ");
@@ -15,6 +16,16 @@ namespace AlgorithamProblem
 
             switch(userInput)
             {
+                case 0:
+                    Console.Write("\nEnter String for Permutation : ");
+                    string str = Console.ReadLine();
+                    int Length = str.Length;
+                    Console.WriteLine("Using Recursion Method Permutations are :: ");
+                    Permutation.RecursionPermutation(str, 0, Length - 1); // using Recursion Menthod
+                    Console.WriteLine("Using Iterative Method Permutations are :: ");
+                    Permutation.IterationPermutation(str); // using iterative Menthod
+                    break;
+
                 case 1:
                     Console.WriteLine("\nEnter the Car Name which you want to search Using Binary Search : ");
                     string cars = Console.ReadLine();
